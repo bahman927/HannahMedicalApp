771a5f06c45d443c4afced1045caa575ecaa2b28
Bahman Dorand <bahman927@gmail.com>
2024-10-16 01:03:07 -0400

    searchPatients updated

--- src/pages/SearchPatients.jsx
+++ src/pages/SearchPatients.jsx
@@ -3,12 +3,13 @@ import  { useState} from 'react';
 const SearchPatients = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [searchSubmitted, setSearchSubmitted] = useState(false) 
   const [bookedDays, setBookedDays] = useState({});
   const [selectedDoctor, setSelectedDoctor] = useState('');
   
   const doctors =  ['Dr. Mina James','Dr. Ben Hyvard','Dr. Hennry ofss','Dr. Albert Robin','Dr. Frank Starr',
                     'Dr. David Starr','Dr. Marry Tagg','Dr. Elli Oniel','Dr. Hannah Pertain','Dr. Anni Arton']
-    
+   let dayBookings = [] 
 
     const handleCancelAppointment = (appointmentToCancel, selectedDay) => {
 
@@ -28,28 +29,35 @@ const SearchPatients = () => {
     
          localStorage.setItem(`bookedDays_${selectedDoctor}`, JSON.stringify(updatedState));
     };
+
+    const handleInputChange = (e) => {
+      setSearchQuery(e.target.value);
+      setSearchSubmitted(false);
+      setSearchResults([]) // Reset searchSubmitted to false when typing starts
+    };
     
   const handleSearch = () => {
-      const results = [];
+      let results = [];
+      
       for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
+        // const key = localStorage.key(i);
   
-        // Check if the key belongs to a doctor's booked days (assuming the pattern `bookedDays_DoctorName`)
-        if (key.startsWith('bookedDays_')) {
-          const doctorName = key.replace('bookedDays_', ''); // Extract doctor's name
-          const doctorPatients = JSON.parse(localStorage.getItem(key)) || {};
+       // Check if the key belongs to a doctor's booked days (assuming the pattern `bookedDays_DoctorName`)
+        // if (key.startsWith('bookedDays_')) {
+        //   const doctorName = key.replace('bookedDays_', ''); // Extract doctor's name
+        //   const doctorPatients = JSON.parse(localStorage.getItem(key)) || {};
       
           doctors.forEach(doctor => {
             const doctorPatients = JSON.parse(localStorage.getItem(`bookedDays_${doctor}`)) || {};
             Object.keys(doctorPatients).forEach(year => {
               Object.keys(doctorPatients[year]).forEach(month => {
                 Object.keys(doctorPatients[year][month]).forEach(day => {
-                  let dayBookings = doctorPatients[year][month][day];
+                    dayBookings = doctorPatients[year][month][day];
                   Object.keys(dayBookings).forEach((dayKey) => {
                     const bookingsArray = dayBookings[dayKey];
-
+                    
                     if (Array.isArray(bookingsArray)) { 
-
+                      // console.log('bookingsArray = ', bookingsArray)
                       bookingsArray.forEach(slot =>{
                         if (slot.patient && 
                           (slot.patient.name.toLowerCase().includes(searchQuery.toLowerCase()) || 
@@ -60,7 +68,7 @@ const SearchPatients = () => {
                             date: `${day}/${month}/${year}`,
                             slot,
                           });
-                          }
+                         }
                       });
                     }
 
@@ -71,8 +79,12 @@ const SearchPatients = () => {
           })
  
           setSearchResults(results);
+          // if (results) setSearchSubmitted(true);
+          results ? setSearchSubmitted(true):setSearchSubmitted(false)
+          results= []
+
         }
-      }
+    
      }
      
     return (
@@ -82,7 +94,7 @@ const SearchPatients = () => {
         className="border p-2 w-64 rounded mr-5 ml-96"
         placeholder="Search by name, family, or phone"
         value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
+        onChange={handleInputChange}
       />
       <button
         className="bg-blue-500 text-white p-2 rounded mt-2 w-24"
@@ -98,22 +110,20 @@ const SearchPatients = () => {
               <strong>Doctor:</strong> {result.doctor} <br />
               <strong>Date:</strong> {result.date} <br />
               <strong>Time Slot:</strong> {result.slot.timeSlot} <br />
-              <strong>Patient:</strong> {result.slot.patient.name} {result.slot.patient.familyName} <br />
+              <strong>Patient:</strong> {result.slot.patient.name } {result.slot.patient.familyName} <br />
               <button 
                 onClick={() => handleCancelAppointment(result.slot.timeSlot, result.day)} 
-                className="cancel-button">
+                className="">
                 Cancel Appointmant
               </button>
             </li>
           ))}
         </ul>
       )}
-      {searchResults.length === 0 && searchQuery && (
-        <p className="mt-4 text-red-500">No results found</p>
+      {searchResults.length === 0 && searchQuery && searchSubmitted && (
+        <p className="mt-10 ml-96 text-red-500">No results found</p>
       )}
     </div>
-  );
-
-  
+  ); 
 }
 export default SearchPatients;
