fe68d4ad242666028745c7344fc9a43da9611956
Bahman Dorand <bahman927@gmail.com>
2025-04-26 23:40:46 -0400

    updated fetchUserAppointments()

--- client/src/pages/AppointmentList.jsx
+++ client/src/pages/AppointmentList.jsx
@@ -28,8 +28,11 @@ const fetchUserAppointments = async () => {
         method: "GET",
         credentials: "include",
         headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json",
+          'Cache-Control': 'no-cache, no-store, must-revalidate',
+                    'Pragma': 'no-cache',
+                    'Expires': '0',
+          // "Content-Type": "application/json",
+          // "Accept": "application/json",
         },
       });
 
