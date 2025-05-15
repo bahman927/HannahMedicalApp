61a6a0bea403dcd5ff9bf5550aa15fb39490f198
Bahman Dorand <bahman927@gmail.com>
2025-05-15 01:43:55 -0400

    update topDoctors.jsx

--- client/src/components/TopDoctors.jsx
+++ client/src/components/TopDoctors.jsx
@@ -10,17 +10,17 @@ const TopDoctors = () => {
       <h1 className='items-center mb-2 text-3xl'>Top Doctors to Book</h1>
       <p className='mb-10'>Simply browse through extensive list of trusted doctors</p>
       {/* <div className='grid w-full grid-cols-5 gap-5 ' > */}
-      <div className='grid w-full grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5' >
+      <div className='grid w-full grid-cols-1 gap-5 p-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5' >
         {doctors.slice(0,10).map((item,index)=>(
           <div onClick={()=> navigate(`/appointment/${item._id}`)} key={index}>
-            <img   className='w-40 h-36 mb-2 cursor-pointer  bg-gray-100
+            <img   className='w-full h-36 mb-2 cursor-pointer  bg-gray-100
              hover:translate-y-[-6px]  
             transition-all duration-400' src={item.image} alt="" />
             <div>
               <span className='text-sm text-green-500'>Available</span>
             </div>
-            <p className='font-serif text-base cursor-pointer'>{item.name}</p>
-            <p className='mb-3 text-sm cursor-pointer '>{item.speciality}</p>
+            <p className='font-serif text-base break-words cursor-pointer'>{item.name}</p>
+            <p className='mb-3 text-sm break-words cursor-pointer'>{item.speciality}</p>
           </div> 
         
         ))}
