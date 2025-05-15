08b3ec93d1d33d5b67ed7f443fa7d7db779da539
Bahman Dorand <bahman927@gmail.com>
2025-05-15 00:48:00 -0400

    update cors in server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const allowedOrigins = ['https://hannahmedicalapp-1.onrender.com'];
 app.use(cors({
   origin: allowedOrigins,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
